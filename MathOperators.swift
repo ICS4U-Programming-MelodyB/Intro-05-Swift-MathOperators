25187afdacabcf46f3f5c0f3ab9826fad3fa5f95
Melody Berhane <melody.berhane@stu.ocsb.ca>
2023-02-12 02:52:24 +0000

    Finished code

---
+++ MathOperators.swift
@@ -0,0 +1,31 @@
+//
+//  MathOperators.swift
+//
+//  Created by Melody Berhane
+//  Created on 2023-2-9
+//  Version 1.0
+//  Copyright (c) 2023 Melody Berhane. All rights reserved.
+//
+//  Prints Hello, World with Style!.
+import Foundation
+
+// Addition .
+print("3 + 8 = ", 3 + 8)
+// Subtraction
+print("4 - 6 = ", 4 - 6)
+// Multiplication.
+print("2 * 5 = ", 2 * 5)
+// Division with ints.
+print("30 / 5 = ", 30 / 5)
+// Division with floats.
+print("5 / 7 = ", 5.0 / 7.0)
+// Division with double.
+print("5 / 7 = ", 5.0 / 7.0)
+// Exponent raised to 2.
+print("6^2 =  ", pow(6.0, 2.0))
+// Exponent raised to 3.
+print("3^3 = ", pow(3.0, 3.0))
+// Square root.
+print("√25 = ", sqrt(25))
+
+
